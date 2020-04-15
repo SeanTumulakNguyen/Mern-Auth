d88c3c092d85fde38612c6591986bae0b311e9c8
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-04-15 01:08:02 -0400

    add middleware and connect db

--- server/server.js
+++ server/server.js
@@ -1,15 +1,40 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
-const app = express()
+const app = express();
+
+// connect to database
+mongoose
+	.connect(process.env.CLOUD_DATABASE, {
+		useNewUrlParser: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+		useCreateIndex: true
+	})
+	.then(() => console.log('DB CONNECTED'))
+	.catch((err) => console.log('DB CONNECTION ERROR', err));
 
 // import routes
-const authRoutes = require('./routes/auth')
+const authRoutes = require('./routes/auth');
+
+// app middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// app.use(cors())
+if ((process.env.NODE_ENV = 'development')) {
+	app.use(cors({ origin: `http://localhost:3000` }));
+}
 
 // middleware
-app.use('/api', authRoutes)
+app.use('/api', authRoutes);
 
-const port = process.env.port || 8000
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+	console.log(`Server is running on port ${port}`);
+});
