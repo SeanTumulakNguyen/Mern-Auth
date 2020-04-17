fce8cb8e316f92f2f1787f53afb4139bc82836b1
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-04-17 18:41:52 -0400

    finish validator, starting user signup

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
+const User = require('../models/user')
+
 exports.signup = (req, res) => {
-    console.log('REQ BODY ON SIGNUP', req.body)
-    res.json({
-        data: 'you hit signup endpoint'
-    })
+    // console.log('REQ BODY ON SIGNUP', req.body)
+    const { name, email, password } = req.body
+    
 }
--- server/validators/index.js
+++ server/validators/index.js
@@ -1,5 +1,12 @@
 const { validationResult } = require('express-validator')
 
 exports.runValidation = (req, res, next) => {
-    const errors
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            error: errors.array()[0].msg
+        })
+    }
+
+    next()
 }
