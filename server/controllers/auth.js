ea7458284afefb61cd688d038d14c91320a0d36e
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-04-19 15:49:18 -0400

    finish auth route and controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -48,18 +48,61 @@ exports.signup = (req, res) => {
 			subject: `Account Activation Link`,
 			html: `
                 <p>Please use the following link to activate your account.</p>
-                <p>${process.env.CLIENT_URL}/auth/activate</p>
+                <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
                 <hr />
                 <p>This email may contain sensitive information</p>
                 <p>${process.env.CLIENT_URL}</p>
             `
-        };
-        
-        sgMail.send(emailData).then(sent => {
-            // console.log('SIGNUP EMAIL SENT', sent)
-            return res.json({
-                message: `Email has been sent to ${email}. Follow the instruction to activate your account.`
-            })
-        })
+		};
+
+		sgMail
+			.send(emailData)
+			.then((sent) => {
+				// console.log('SIGNUP EMAIL SENT', sent)
+				return res.json({
+					message: `Email has been sent to ${email}. Follow the instruction to activate your account.`
+				});
+			})
+			.catch((err) => {
+				// console.log('SIGNUP EMAIL SENT ERROR', err)
+				return res.json({
+					message: err.message
+				});
+			});
 	});
+};
+
+exports.accountActivation = (req, res) => {
+	const { token } = req.body;
+
+	if (token) {
+		jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, function (err, decoded) {
+			if (err) {
+				console.log('JWT VERIFY IN ACCOUNT ACCIVATION ERROR', err);
+				return res.status(401).json({
+					error: 'Expired link. Sign up again.'
+				});
+			}
+
+			const { name, email, password } = jwt.decode(token);
+
+			const user = new User({ name, email, password });
+
+			user.save((err, user) => {
+				if (err) {
+					console.log('SAVE USER IN ACCOUNT ACTIVATION ERROR', err);
+					return res.status(401).json({
+						error: 'Error saving user in database. Try sign up again.'
+					});
+				}
+				return res.json({
+					mesasge: 'Sign up success. Please sign in.'
+				});
+			});
+		});
+	} else {
+		return res.json({
+			message: 'Something went wrong. Try again.'
+		})
+	}
 };
