63447176a6db7d3f99e28ee5c3f92301ca57248f
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-04-24 00:29:50 -0400

    start form for signup

---
+++ client/src/auth/Signup.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import { Link, Redirect } from 'react-router-dom'
+import Layout from '../core/Layout'
+import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.min.css'
+
+const Signup = () => {
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        buttonText: 'Submit'
+    })
+
+    const { name, email, password, buttonText } = values
+
+    const handleChange = (name) => (e) => {
+        e.preventDefault()
+
+    }
+
+    const clickSubmit = (e) => {
+
+    }
+
+    const signupForm = () => (
+        <form>
+            <div className='form-group'>
+                <label className='text-muted'>Name</label>
+                <input onChange={handleChange('name')} value={name} type='text' className='form-control' />
+            </div>
+            <div className='form-group'>
+                <label className='text-muted'>Email</label>
+                <input onChange={handleChange('email')} value={email} type='text' className='form-control' />
+            </div>
+            <div className='form-group'>
+                <label className='text-muted'>Password</label>
+                <input onChange={handleChange('password')} value={password} type='password' className='form-control' />
+            </div>
+            <div>
+                <button className='btn btn-primary float-right' onClick={clickSubmit}>{buttonText}</button>
+            </div>
+        </form>
+    )
+
+    return (
+        <Layout>
+            <div className='col-md-6 offset-md-3'>
+            <ToastContainer />
+            <h1 className='p-5 text-center'>Signup</h1>
+            {signupForm()}
+            </div>
+        </Layout>
+    )
+}
+
+export default Signup
